94fb00736a088db26d071ae0450901c4737b6f5d
amazed11 <amazedking2@gmail.com>
2022-05-14 15:09:49 +0545

    new code for api

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const path = require("path");
 const app = express();
 const port = 3000;
 app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
 app.use(
   express.urlencoded({
     extended: true,
