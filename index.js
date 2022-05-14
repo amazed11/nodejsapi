dacb54bb8e8c7fc399bf6a74bd6a2b00d6765d60
amazed11 <amazedking2@gmail.com>
2022-05-14 14:57:41 +0545

    path fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const pool = require("mysql2");
 const multer = require("multer");
+const path = require("path");
 const app = express();
 const port = 3000;
 app.use(express.json());
